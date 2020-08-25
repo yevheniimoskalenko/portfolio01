843445108843acfcf05a12f5b85cd3a5f1436184
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-08-25 18:33:23 +0300

    sectioon header

---
+++ components/header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="header">
+    <div class="background">
+      <div class="profile">
+        <div class="avatar">
+          <el-avatar :size="150" :src="url" />
+        </div>
+        <div class="name">
+          <span>Yevhenii Moskalenko</span>
+          <small>Full stack vue.js & node.js</small>
+          <p>
+            Hi, my name is Yevhenii, I`am from Ukraine, I live in city Kharkiv,
+            I 23 years, I like web development, namely vue.js and node.js
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//
+export default {
+  data() {
+    return {
+      url:
+        'https://res.cloudinary.com/dmrpkseez/image/upload/v1598355171/image_ahstla.png'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+}
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: url('~assets/header.jpg') no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  .background {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background: #29292a48;
+  }
+  .profile {
+    .name {
+      color: #fff;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      span {
+        font-size: 72px;
+        font-family: 'Bebas Neue', cursive;
+        line-height: 73px;
+        &::after {
+          content: '';
+          display: flex;
+          margin: auto;
+          width: 150px;
+          height: 2px;
+          background: #192624;
+        }
+      }
+      small {
+        line-height: 50px;
+        font-size: 36px;
+        font-family: 'Quicksand', sans-serif;
+      }
+    }
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,14 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300&family=Quicksand&display=swap'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -23,7 +30,11 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'normalize.css/normalize.css',
+    '@/theme/index.scss'
+  ],
   /*
    ** Plugins to load before mounting the App
    */
